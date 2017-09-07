68fe524c99ef4cce1300ec385d0412485c41ebbb
Dakota Weber <dakota.weber01@gmail.com>
2017-09-07 12:52:31 -0600

    Added Runner and Controller and De-bugged them.

--- src/process/controller/ProjectController.java
+++ src/process/controller/ProjectController.java
@@ -3,4 +3,9 @@ package process.controller;
 public class ProjectController
 {
 
+	public void start()
+	{
+		System.out.println("This is a project there are many");
+		System.out.println("Like it but this one is mine");
+	}
 }
--- src/process/controller/ProjectRunner.java
+++ src/process/controller/ProjectRunner.java
@@ -2,5 +2,10 @@ package process.controller;
 
 public class ProjectRunner
 {
-
+	public static void main (String [] args)
+	{
+	ProjectController app = new ProjectController();
+	app.start();
+	}
 }
+	
